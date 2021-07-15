f9349f310d5a5d4b5e9f368c4102bbcf385c87cd
Matthew Stibbard <matthewstibbard@gmail.com>
2021-07-15 22:34:39 +1000

    authentication working with Google

---
+++ components/Login.tsx
@@ -0,0 +1,36 @@
+import React, { ReactElement } from 'react';
+import firebase from '../firebase/firebaseClient';
+
+interface LoginProps {}
+
+export const Login = ({}: LoginProps) => {
+  async function signInWithGoogle() {
+    const userCredentials = await firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+    console.log({ ...userCredentials.user });
+
+    firebase.firestore().collection('users').doc(userCredentials.user.uid).set({
+      uid: userCredentials.user.uid,
+      email: userCredentials.user.email,
+      name: userCredentials.user.displayName,
+      provider: userCredentials.user.providerData[0].providerId,
+      photoUrl: userCredentials.user.photoURL,
+      createdAt: new Date(),
+    });
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => signInWithGoogle()}
+        className='bg-green-400 hover:bg-green-500 rounded p-2'
+      >
+        Sign in with Google
+      </button>
+    </>
+  );
+};
+
+export default Login;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,27 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Login from '../components/Login';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Next.js + Firebase + Typescript + Tailwind + Stripe</title>
-        <meta name="description" content="Ready-to-use project built with Next.js + Firebase + Typescript + Tailwind + Stripe" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name='description'
+          content='Ready-to-use project built with Next.js + Firebase + Typescript + Tailwind + Stripe'
+        />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className='container mx-auto max-w-4xl'>
-
         <h1 className='mt-10 mb-10 text-2xl font-extrabold py-2'>
           Next.js + Firebase + Typescript + Tailwind + Stripe
         </h1>
 
-        <p className='py-2'>
-          Get started quickly with this!
-        </p>
+        <p className='py-2'>Get started quickly with this!</p>
+
+        <Login />
       </main>
     </>
-  )
+  );
 }
